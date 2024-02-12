c3d22e451697726ee43a19292b6a412d31e1d4da
VeeSimz <kayveesimba@gmail.com>
2024-02-12 21:41:38 +0200

    Fixed pycodestyle in console file

--- console.py
+++ console.py
@@ -2,19 +2,25 @@
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
+    def emptyline(self):
+        """Empty line"""
+        pass
+
     def do_quit(self, line):
-        """Quits the program"""
-        print()
+        """Quit command to exit the program"""
+        print("")
         return True
 
     def do_EOF(self, line):
         """Exits the program"""
         print()
         return True
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
