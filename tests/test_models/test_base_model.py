5eebef13f24b7173699372bb0b1981c3996ac086
Mariam <kelanimaryam1234@gmail.com>
2024-02-13 00:55:07 -0800

    tests folder updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python3
+""" Defines unnitest framework for each test suite """
 import unittest
 from datetime import datetime, timedelta
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
+    """ Unnitest initialization for the class area """
+
+
      def setUp(self):
          self.real_model = BaseModel()
 
