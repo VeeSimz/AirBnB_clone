5d666f2ea4e17cff81d60da622255e63125e61ab
VeeSimz <kayveesimba@gmail.com>
2024-02-14 04:41:02 +0200

    Fixed pycodestyle errors

--- console.py
+++ console.py
@@ -4,8 +4,8 @@ import cmd
 import sys
 from models.base_model import BaseModel
 
-class HBNBCommand(cmd.Cmd):
 
+class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) " if sys.__stdin__.isatty() else ""
 
@@ -23,16 +23,6 @@ class HBNBCommand(cmd.Cmd):
         """Exits the program"""
         print("")
         exit()
-
-    def do_create(self, line):
-        if not line:
-            print("** class name missing **")
-        elif line != "BaseModel"
-            print("** class doesn't exist **")
-        else:
-            new_model = BaseModel()
-            new_model.save
-            print(new_model.id)
 
 
 if __name__ == '__main__':
